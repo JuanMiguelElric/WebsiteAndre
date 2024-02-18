3657e4097aa2cc41c92821af1a85fcc15f6d14a1
JuanMiguelElric <oliveirajuan243@gmail.com>
2024-02-18 00:53:29 -0300

    primeira parte do website desenvolivda com sucesso

--- resources/js/Pages/Welcome.jsx
+++ resources/js/Pages/Welcome.jsx
@@ -3,13 +3,14 @@ import Menu from '@/Components/menu';
 import { Link, Head } from '@inertiajs/react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../../css/index.css';
+import PrimaryParty from '@/Components/primaryparty';
 export default function Welcome({ auth, laravelVersion, phpVersion }) {
     return (
         <>
             <Head title="André Luiz Personal" />
             <div class="primarybackground">
                 <Menu />
-                <h3>dddddddddddddd</h3>
+                <PrimaryParty />
             </div>
 
         </>
